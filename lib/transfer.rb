0d6e39f4bdfd5bfefa9ae2c5e3dcce32d184dd80
Alex McKean <amckean@me.com>
2017-11-30 19:59:18 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,8 +30,14 @@ class Transfer
     else
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
-    end
-        
+    end    
   end
 
+  def reverse_transfer
+    if self.status = "complete"
+      self.sender.balance += amount
+      self.receiver.balance -= amount
+      self.status = "reversed"
+    end 
+  end   
 end
