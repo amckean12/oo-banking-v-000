6851d985aa0f434ec1f7ce286a88648432b74b55
Alex McKean <amckean@me.com>
2017-11-30 17:39:56 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -8,4 +8,17 @@ class BankAccount
     @balance = 1000
     @status = "open"
   end
+
+  def deposit(amount)
+    @balance = @balance + amount
+  end
+
+  def display_balance
+  end
+
+  def valid?
+  end
+
+  def close_account 
+  end
 end
